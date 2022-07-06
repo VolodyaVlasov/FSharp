952403be234fe7fb29de1069a9f400b7622752c9
Vladimir <51863950+VolodyaVlasov@users.noreply.github.com>
2022-07-06 16:54:57 +0300

    Update fourth.fs

--- FSharp/fourth.fs
+++ FSharp/fourth.fs
@@ -3,4 +3,12 @@ let rec fibo = function
     | n when n <= 2 -> 1
     | n -> fibo(n - 1) + fibo(n - 2)
     
+let rec sum = function
+    | n when n = 0 -> 0
+    | n when n = 1 -> 1
+    | n -> n + sum(n - 1)
+    
+let rec sum2 = function 
+ | (m,0) -> m 
+ | (m,n) -> m + n + sum2(m, n - 1)    
     
