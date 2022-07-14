79271ec41bb2e2c7c53657aec885327f08fe6036
Vladimir <51863950+VolodyaVlasov@users.noreply.github.com>
2022-07-14 19:08:17 +0300

    Update five.fs

--- FSharp/five.fs
+++ FSharp/five.fs
@@ -1,1 +1,10 @@
+let notDivisible (n, m) =  m % n = 0
 
+let rec check = function
+   | n, d when d > n / 2 -> true
+   | n, d when n % d = 0 -> false
+   | n, d -> check(n, d + 1)
+       
+let  prime = function
+   | 1 | 2 -> true
+   | n -> check (n , 2)
